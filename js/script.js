c66417d1d0c8e25c831738ff821bc252bf1900f7
Klipsoman <68691573+Klipsoman@users.noreply.github.com>
2021-04-17 15:37:30 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,37 +1,17 @@
-
+// vars
 let menu = document.querySelector(".menu");
-let burger = document.querySelector("#burger");
-let burgerSpan1 = document.querySelector('bar1');
-let burgerSpan2 = document.querySelector('bar2');
-let burgerSpan3 = document.querySelector('bar3');
-let slide = document.querySelector('.s1');
-let slides = document.querySelector('.slides');
+let burger = document.getElementById("burger");
+let burgerSpan1 = document.querySelector('.bar1');
+let burgerSpan2 = document.querySelector('.bar2');
+let burgerSpan3 = document.querySelector('.bar3');
+let bars = [burgerSpan1, burgerSpan2, burgerSpan3]
 
+burger.addEventListener('click', openMenu)
 
-burger.onclick = function openMenu(){
-    menu.classList.toggle('showmenu');
-    burgerSpan1.classList.toggle('change');
-    burgerSpan2.classList.toggle('change');
-    burgerSpan3.classList.toggle('change');
-    burger.classList.toggle('fix');
+function openMenu(){
+	burger.classList.toggle('fix');
+	menu.classList.toggle('showmenu');
+	for(let bar of bars){
+		bar.classList.toggle('change')
+	}
 }
-
-
-let cliXdown = 0;
-let cliXup = 0;
-
-slides.addEventListener('mousedown', function(event){
-   cliXdown = event.clientX;
-})
-slides.addEventListener('mouseup', function(event){
-  cliXup = event.clientX;
-  if (cliXup < cliXdown) {
-    slide.style.marginLeft -= 33 + '%';
-  } 
-  else if (cliXup > cliXdown) {
-    slide.style.marginLeft += 33 + '%';
-  } 
-})
-
-
-
